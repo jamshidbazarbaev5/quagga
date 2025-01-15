1e24ed292b70f57c00eabc0cd7118be621212f93
jamshidbazarbaev5 <your-github-email@example.com>
2025-01-16 02:53:49 +0500

    fixed not found error

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,3 +4,24 @@ export const api =axios.create({
     baseURL:'https://easybonus.uz/api'
 
 })
+
+
+
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response?.status === 401) {
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
