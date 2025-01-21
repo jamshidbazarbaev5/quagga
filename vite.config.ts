179e22bc1954ef102efc59424a174bb738ed3c83
jamshidbazarbaev5 <your-github-email@example.com>
2025-01-21 15:25:44 +0500

    fixed error for navigating login

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {VitePWA} from "vite-plugin-pwa";
-
 
 export default defineConfig({
-   plugins: [react(),
-     VitePWA({
-       registerType: 'autoUpdate',
-       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-       manifest: {
-         name: 'EasyBonus',
-         short_name: 'EasyBonus',
-         theme_color: '#ffffff',
-         icons: [
-           {
-             src: 'pwa-64x64.png',
-             sizes: '64x64',
-             type: 'image/png'
-           },
-           {
-             src: 'pwa-192x192.png',
-             sizes: '192x192',
-             type: 'image/png'
-           },
-           {
-             src: 'pwa-512x512.png',
-             sizes: '512x512',
-             type: 'image/png',
-             purpose: 'any'
-           },
-           {
-             src: 'maskable-icon-512x512.png',
-             sizes: '512x512',
-             type: 'image/png',
-             purpose: 'maskable'
-           }
-         ],
-       },
-     })
-
-
-   ],
+  plugins: [react()],
   css: {
     postcss: './postcss.config.js',
   },
   base: '/',
   server: {
-   
+
   },
   build: {
     rollupOptions: {
@@ -55,5 +17,4 @@ export default defineConfig({
       },
     },
   },
-
 })
